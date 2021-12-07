b37365f7b0adc3f5cb780d67cc28db8cfc24f987
ik_sunweb <ik@sunweb.pro>
2021-12-07 20:38:18 +0500

    hw-6

--- react-app/src/redux/store.ts
+++ react-app/src/redux/store.ts
@@ -1,16 +1,44 @@
-import {createStore} from 'redux';
+import {combineReducers, createStore} from 'redux';
 
 const initialState = true;
+const initialChats = {
+    1: {
+        chat_id: 1,
+        chat_name: 'Main',
+        chat_messages: []
+    },
+    2: {
+        chat_id: 2,
+        chat_name: 'Second',
+        chat_messages: []
+    }
+}
 export const actionChecked = () => {
     return {
         type: 'CHECKED'
     }
 }
 
+export const actionAddMessage = () => {
+    return {
+        type: 'ADD_MESSAGE'
+    }
+}
+
+
+
 const counterReducer = (state:boolean = initialState, action:any) => {
     switch (action.type) {
         case 'CHECKED':
             state = !state;
+    }
+    return state;
+}
+
+const chatReducer = (state:object = initialChats, action:any, data:any) => {
+    switch (action.type) {
+        case 'ADD_MESSAGE':
+            Object.values(state).filter((chat) => chat.chat_id == data.chat_id);
     }
     return state;
 }
